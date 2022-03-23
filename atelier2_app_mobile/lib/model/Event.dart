cb1fe97ca579b392483fc8561975b625af5dbd5d
Jules <jfrancois968@gmail.com>
2022-03-23 16:46:15 +0100

    💩 Test change icon

--- atelier2_app_mobile/lib/model/Event.dart
+++ atelier2_app_mobile/lib/model/Event.dart
@@ -3,18 +3,16 @@ import 'package:dio/dio.dart';
 import 'User.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-
 //part 'Event.g.dart';
 
-
 @JsonSerializable()
-//TODO test if we can remove @JsonKey(name: 'id_events') 
+//TODO test if we can remove @JsonKey(name: 'id_events')
 //when the name is the name
 class Event {
   @JsonKey(name: 'id_events')
-  late int id; 
+  late int id;
   @JsonKey(name: 'token')
-  late String token; 
+  late String token;
   @JsonKey(name: 'title')
   late String title;
   @JsonKey(name: 'address')
@@ -30,7 +28,7 @@ class Event {
   @JsonKey(name: 'token')
 
   //Constructor
-  Event(this.title){
+  Event(this.title) {
     id = -1;
     token = "-1";
     address = "Nancy";
@@ -38,7 +36,6 @@ class Event {
     authorId = -1;
     dateEvent = DateTime.now();
     updatedAt = DateTime.now();
-
   }
 
   factory Event.fromJson(Map<String, dynamic> json) {
@@ -53,31 +50,28 @@ class Event {
     return e;
   }
 
-  double getLat(){
-    print("loc : "+ location);
+  double getLat() {
+    print("loc : " + location);
     int endIndex = location.indexOf(",");
-    String sx = location.substring(0,  endIndex);
-    print("sx : "+sx);
+    String sx = location.substring(0, endIndex);
+    print("sx : " + sx);
     return double.parse(sx);
   }
 
-  double getLong(){
+  double getLong() {
     int startIndex = location.indexOf(", ");
-    String sy = location.substring(startIndex+2,  location.length);
-    print("sy : "+sy);
+    String sy = location.substring(startIndex + 2, location.length);
+    print("sy : " + sy);
     return double.parse(sy);
   }
 
-
   ///TODO
-  getOrganisateurName(){
+  getOrganisateurName() {
     return "Jules";
   }
 
-
   //TODO ajouter user à un èvent
-  join(User u1){
-  }
+  join(User u1) {}
 
   /// Call our api to create the event in db
   /// TODO set author id
@@ -88,11 +82,17 @@ class Event {
       var response = await Dio()
           //true data
           //.post('http://docketu.iutnc.univ-lorraine.fr:62345/events/create', data: {'title': title, 'address': address, 'localisation':location,'date_events': dateEvent.toString(), 'user_id_user': 1});
-          .post('http://docketu.iutnc.univ-lorraine.fr:62345/events/create', data: {'title': "play together", 'address': "ici", 'localisation':"48.2 6.2",'date_events': "2022-12-12 00:00:00", 'user_id_user': 1});
+          .post('http://docketu.iutnc.univ-lorraine.fr:62345/events/create',
+              data: {
+            'title': "play together",
+            'address': "ici",
+            'localisation': "48.2 6.2",
+            'date_events': "2022-12-12 00:00:00",
+            'user_id_user': 1
+          });
       print(response);
     } catch (e) {
       print(e);
     }
   }
-
-}
+} 

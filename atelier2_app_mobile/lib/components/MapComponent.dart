801d83efcb3399482e843e9255bfdcd68b560626
Jules <jfrancois968@gmail.com>
2022-03-30 16:43:25 +0200

    Revert "Revert "Revert "💩 test"""
    This reverts commit de360e038b730e651bb77b47a51bc34cef90f534.

--- atelier2_app_mobile/lib/components/MapComponent.dart
+++ atelier2_app_mobile/lib/components/MapComponent.dart
@@ -9,9 +9,9 @@ import "package:latlong2/latlong.dart" as lt;
 
 // ignore: must_be_immutable
 class MapComponent extends StatefulWidget {
-  const MapComponent({Key? key, required this.event}) : super(key: key);
+  const MapComponent({Key? key, required this.events}) : super(key: key);
 
-  final Event event;
+  final List<Event> events;
   
   @override
   State<MapComponent> createState() => _MapComponentState();
@@ -23,20 +23,23 @@ class _MapComponentState extends State<MapComponent> {
   Widget build(BuildContext context) {
     List<Marker> mark = [];
 
-
+      ///on stocke widget.events dans une variable temporaire pour bien lui faire
+      ///comprendre que la valeur n'est pas nul, cf if au dessus
+      List<Event> tmp = widget.events;
+      for (var event in tmp) {
+        print("ma boy");
         mark.add(Marker(
           width: 80.0,
           height: 80.0,
-          //point: lt.LatLng(widget.event.getLat(), widget.event.getLong()),
-          point: lt.LatLng(0,0),
+          point: lt.LatLng(event.getLat(), event.getLong()),
           builder: (ctx) =>
               const Icon(Icons.location_pin, color: Colors.indigoAccent),
         ));
+    }
 
     return FlutterMap(
       options: MapOptions(
-        //center: lt.LatLng(widget.event.getLat(), widget.event.getLong()),
-        center: lt.LatLng(0,0),
+        center: lt.LatLng(51.5, -0.09),
         zoom: 14.0,
       ),
       layers: [
--- atelier2_app_mobile/lib/screens/OneEvent.dart
+++ atelier2_app_mobile/lib/screens/OneEvent.dart
@@ -46,6 +46,9 @@ class _OneEventState extends State<OneEvent> {
     double bottomPadding = MediaQuery.of(context).padding.bottom;
     widget.getEventDatas();
 
+    List<Event> evtList = [];
+    evtList.add(widget.event);
+    print("evt : " + evtList.toString());
     return Scaffold(
         //TODO test if we can remove app Bar here (necessary ?)
         appBar: AppBar(
@@ -72,7 +75,7 @@ class _OneEventState extends State<OneEvent> {
                     child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          //Expanded(child: MapComponent(event: widget.event)),
+                          Expanded(child: MapComponent(events: evtList)),
                           const SizedBox(height: 18),
                         ])),
                 ListTile(
